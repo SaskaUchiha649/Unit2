c5e03c615e79cc5491203004fce3fdbb809f50fa
Zachary Vasquez <vasquezz@roverkids.org>
2025-05-02 14:16:12 -0400

    finished 2.3

--- AnimalStampede/AnimalStampede/Assets/Scripts/SpawnManager.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +16,9 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
