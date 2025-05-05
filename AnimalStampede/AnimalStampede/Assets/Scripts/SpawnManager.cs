83787b65cc16a8ba6e554bb5ca36e03c31a7b9c5
Zachary Vasquez <vasquezz@roverkids.org>
2025-05-05 14:17:48 -0400

    finished 2.4

--- AnimalStampede/AnimalStampede/Assets/Scripts/SpawnManager.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/SpawnManager.cs
@@ -8,17 +8,21 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+
     }
+    void SpawnRandomAnimal()
+    {
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    
 }
